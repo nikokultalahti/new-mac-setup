5aab1106e43236f87827e884e3c7885f61249545
Niko Kultalahti <27192805+nikokultalahti@users.noreply.github.com>
2023-11-07 10:22:49 +0200

    Added work related apps

--- install-apps.sh
+++ install-apps.sh
@@ -11,25 +11,27 @@ code-select --install
         eval $("/opt/homebrew/bin/brew" shellenv)
     fi
 
-# Install System Apps
+# Install CLI tools
 
-brew install mas                        # For App Store Automation
 brew install git                        # Git 
-brew install node                       # NodeJS
-brew install bitwarden-cli              # Command-line tools for Bitwarden (for Raycast integration)
+brew install nvm                        # Node version manager
+brew install mas                        # For App Store Automation
+brew install 1password-cli              # Command-line tools for 1Password
 brew install zsh-syntax-highlighting    # Syntax highlightning for zsh
 brew install zsh-autosuggestions        # Autosuggestions for zsh
 
 # Install App Store apps
 
-mas install 1352778147                  # Bitwarden
 mas install 409201541                   # Pages
 mas install 409203825                   # Numbers
+mas install 409183694                   # Keynote
 mas install 409203825                   # Subtrack
 mas install 1659154653                  # Mona for Mastodon
 mas install 1289583905                  # Pixelmator Pro
 mas install 775737590                   # iA Writer
+mas install 1032670789                  # NewsExplorer
 mas install 1474335294                  # Goodlinks
+mas install 1006087419                  # SnippetsLab
 mas install 747648890                   # Telegram
 mas install 1440147259                  # Adguard
 mas install 1475387142                  # Tailscale
@@ -39,19 +41,23 @@ mas install 1475387142                  # Tailscale
 mas install 6443941139                  # 2FAS Authentication Extension
 mas install 1606897889                  # Consent-O-Matic
 mas install 1494051017                  # SimpleLogin
+mas install 1569813296                  # 1Password for Safari
 
 # Install Non-App Store Apps
 
-brew install --cask netnewswire         # NetNewsWire
 brew install --cask signal              # Signal
 brew install --cask vscodium            # VS Code without telemetry
 brew install --cask calibre             # Calibre
 brew install --cask raycast             # Raycast
-brew install --cask vnc-viewer          # VNC Viewer
 brew install --cask mullvadwpn          # Mullvad VPN
 brew install --cask iterm2              # iTerm2
 
 # Other Apps
 
 ruby gem install colorls                # Colorls for iTerm2 & ZSH
+
+# For Work
+
+mas install 803453959                   # Slack
+mas install 1298450641                  # monday.com
 
