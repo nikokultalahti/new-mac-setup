4b6960c9f9c32986a2e6da69848013b76d2c48cf
Niko Kultalahti <27192805+nikokultalahti@users.noreply.github.com>
2024-03-26 13:25:56 +0200

    updated list

--- install-apps.sh
+++ install-apps.sh
@@ -29,18 +29,28 @@ mas install 409203825                   # Subtrack
 mas install 1659154653                  # Mona for Mastodon
 mas install 1289583905                  # Pixelmator Pro
 mas install 775737590                   # iA Writer
-mas install 1529448980                  # Reeder 5
 mas install 1006087419                  # SnippetsLab
 mas install 747648890                   # Telegram
 mas install 1440147259                  # Adguard
 mas install 1475387142                  # Tailscale
+mas install 904280696                   # Things 3
+mas install 1607635845                  # Velja
+mas install 1496543317                  # Book Tracker
+mas install 1545870783                  # Color Picker
+mas install 1471867429                  # OTP Auth
+mas install 441258766                   # Magnet
+mas install 1474335294                  # GoodLinks
+
 
 # Install Safari Extensions
 
-mas install 6443941139                  # 2FAS Authentication Extension
 mas install 1606897889                  # Consent-O-Matic
 mas install 1494051017                  # SimpleLogin
 mas install 1569813296                  # 1Password for Safari
+mas install 6471380298                  # StopTheMadness
+mas install 1514703160                  # Focus for Youtube
+mas install 1440147259                  # AdGuard for Safari
+mas install 6446224821                  # StreetPass for Mastodon
 
 # Install Non-App Store Apps
 
@@ -53,3 +63,12 @@ brew install --cask iterm2              # iTerm2
 brew install --cask iina                # IINA
 brew install --cask 1password           # 1Password
 brew install --cask cleanshot           # CleanShotX
+brew install --cask arq                 # Arq Backup7
+brew install --cask diffusionbee        # DiffusionBee
+brew install --cask hiddenbar           # Hidden Bar
+brew install --cask keyclu              # KeyClu
+brew install --cask logi-options-plus   # Logi Options+
+brew install --cask meetingbar          # MeetingBar
+brew install --cask netnewswire         # NetNewsWire
+brew install --postman                  # Postman  
+brew install --pycharm-ce               # Pycharm
