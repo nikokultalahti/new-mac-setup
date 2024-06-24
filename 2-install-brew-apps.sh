11b3ad3d9aa8f5bcef854a6963f80801c947597c
Niko Kultalahti <27192805+nikokultalahti@users.noreply.github.com>
2024-06-24 12:42:43 +0300

    Clean

--- 2-install-brew-apps.sh
+++ 2-install-brew-apps.sh
@@ -67,13 +67,9 @@ brew install --cask rectangle               # Rectangle
 # Install development tools                                                   #
 ###############################################################################
 
-<<<<<<< HEAD
 brew install python3                    # Python
 brew install nvm                        # Node version manager
-=======
-brew install python3                        # Python
 brew install --cask git-credential-manager  # Git credential manager
->>>>>>> 04aaf1ab6156d5bef878f1ec8d11830ba5ce941d
 
 # Remove outdated versions from the Homebrew cellar.
 brew cleanup
